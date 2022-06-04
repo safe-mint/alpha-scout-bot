21425b81fece31a1499639212a77769fe52b45dc
_safeMint <safemintyyx@gmail.com>
2022-06-04 15:50:17 -0500

    better error message examples

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,20 @@ function messageUsername(message:DiscordJS.Message<boolean>) {
   return message.author.username + "#" + message.author.discriminator
 }
 
-const EXAMPLES = "for example\nhttps://twitter.com/ChunkyChihuahuas\nor\nhttps://mobile.twitter.com/GrumpyGremplins\n\nif you know the estimated launch date, you can add it after the twitter link, like \nhttps://mobile.twitter.com/PuffyPandas September 14, 2023"
+const EXAMPLES = "for example\nhttps://twitter.com/ChunkyChihuahuas\nor\nhttps://mobile.twitter.com/GrumpyGremplins\n\nIf you know the estimated launch date, you can add it after the twitter link, like \nhttps://mobile.twitter.com/PuffyPandas September 14, 2023\nor\nhttps://mobile.twitter.com/OrangeOtters Q3 2023"
 
 client.on('messageCreate', async (message) => {
   if (message.author.bot) return;
   const handler = new MessageHandler()
   const result = await handler.handle(message.content, messageUsername(message))
   if (result == MessageHandler.STATUS.BAD_TWITTER_LINK) {
-    message.reply({ content: `invalid format: please start with a Twitter link (i.e. it should start with https://www.twitter.com/)\n${EXAMPLES}`})
+    message.reply({ content: `Invalid Format: Please enter the project Twitter link (i.e. it should start with https://www.twitter.com/)\n${EXAMPLES}`})
   } else if (result === MessageHandler.STATUS.DB_SUCCESS) {
-    message.reply({ content: "thank you! successfully saved" })
+    message.reply({ content: "Thank You! Successfully Saved" })
   } else if (result === MessageHandler.STATUS.DUPLICATE_RECORD) {
-    message.reply({ content: "that NFT project has already been added"})
+    message.reply({ content: "That NFT project has already been added"})
   } else if (result == MessageHandler.STATUS.DB_SAVING_ERROR) {
-    message.reply({ content: "ERROR saving to the database, please contact admin"})
+    message.reply({ content: "ERROR saving to the database, please contact an admin"})
   }
 
 })
